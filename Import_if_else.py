b38583c02619b16003ecedef084fdb91b3c7becc
mansi-borole <98953334+mansi-borole@users.noreply.github.com>
2022-07-29 23:56:10 +0530

    Add files via upload

---
+++ Import_if_else.py
@@ -0,0 +1,93 @@
+'''x,x=20,30
+
+y,y= x+1,x+2
+
+print(x,y)'''
+
+'''a=3
+
+b=2
+
+c=a**b
+
+print(c)'''
+
+'''a = int(input("enter rad :"))
+
+print(a)
+
+print(type(a))'''
+
+'''a,b,c=10,20,3+4j
+
+print(type(a))
+
+print(type(b))
+
+print(type(c))'''
+
+# print(bool("1"))
+
+
+'''a="computer"
+
+print(a[0])
+
+print(a[1])
+
+print(a[3])
+
+print(a[5])
+
+print(a[7]
+
+'''
+
+'''import math as m
+
+b = m.pow(3,2)
+
+sq = m.sqrt(81)
+
+print(b)
+
+print(sq)'''
+
+'''a=r.randint(2,10)
+
+print(a)'''
+
+'''import random as r
+
+a=int(input("enter the value: "))
+
+b=int(input("enter the value: "))
+
+c=r.randint(a,b)
+
+print(c)'''
+
+'''import random as r
+
+#=int(input("enter the value: "))
+
+#b=int(input("enter the value: "))
+
+#c=int(input("enter the value: "))
+
+d=r.randrange(5,15)
+
+print(d)'''
+
+import random as r
+
+a = r.randrange(1, 100)
+
+b = r.randrange(1, 100)
+
+c=r.randrange(1, 100)
+
+print(a, b, c)
+
+
+
