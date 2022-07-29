2d85b187ca845f6483d05e6558568a08de3dca92
mansi-borole <98953334+mansi-borole@users.noreply.github.com>
2022-07-29 23:58:58 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,83 @@
+'''num1=int(input("enter num1 : "))
+
+if num1>=18:
+
+    print(f"{num1} you are eligible for voting!!")
+
+else:
+
+    print(f"{num1} You'r not eligible fpr voting!!")
+
+    a=18-num1
+
+    print(f"After {a} year you can vote!!")'''
+
+'''n1=int(input("enter num1 : "))
+
+if n1%3==0 and n1%5==0:
+
+    print(f"{n1} is divisible by 3 and 5")
+
+else:
+
+    print(f"{n1} not divisible by 3 and 5")
+
+'''
+
+'''n1=int(input("enter num1 : "))
+
+n2=int(input("enter num1 : "))
+
+n3=int(input("enter num1 : "))
+
+if n1>=n2 and n1>=n3:
+
+    print(f"{n1} is greater")
+
+elif n2>=n1 and n2>=n3:
+
+    print(f"{n2} is greater")
+
+else:
+
+    print(f"{n3} is greater")'''
+
+'''n1=int(input("enter num1 : "))
+
+if n1%2==0:
+
+    print("even")
+
+else:
+
+    print("Odd")'''
+
+'''n1=int(input("enter num1 : "))
+
+if n1>0:
+
+    print("positive")
+
+elif n1<0:
+
+    print("Negative")
+
+else:
+
+    print("Zero")
+
+'''
+
+n1=int(input("enter num1 : "))
+
+if n1==1 or n1==3 or n1==5 or n1==7 or n1==8 or n1==10 or n1==12:
+
+    print("31")
+
+elif n1==3 or n1==4 or n1==6 or n1==9 or n1==11:
+
+    print("30")
+
+elif n1==2:
+
+    print("28")
