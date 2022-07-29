77aa75881efc3f2a7bd7d75325b3d475ff469176
mansi-borole <98953334+mansi-borole@users.noreply.github.com>
2022-07-29 23:58:24 +0530

    Add files via upload

---
+++ If_else.py
@@ -0,0 +1,13 @@
+num1=int(input("enter num1 : "))
+
+if num1>=18:
+
+    print(f"{num1} you are eligible for voting!!")
+
+else:
+
+    print(f"{num1} You'r not eligible fpr voting!!")
+
+    a=18-num1
+
+    print(f"After {a} year you can vote!!")
